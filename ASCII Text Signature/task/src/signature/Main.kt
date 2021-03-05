1b36938f09a3821e0a5d11afa6ae129753203dc6
NikoBolt <docnikbolt@gmail.com>
2021-03-05 21:45:08 +0300

    Stage 2/4: Star quality

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,9 +1,41 @@
 package signature
+
+import java.util.*
+
+/*
+Stage 2/4: Star quality
+ */
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val str = scanner.nextLine()
+
+    printStr1(str)
+}
+
+fun printStr1(str: String) {
+    val len = str.length
+    var output = ""
+
+    repeat(len + 4) {
+        output += '*'
+    }
+    output += "\n* "
+    output += str
+    output += " *\n"
+    repeat(len + 4) {
+        output += '*'
+    }
+    print(output)
+}
+
+
+
 /*
 Stage 1/4: Primitive Art
- */
+
 fun main() {
     println(" _____________\n" +
             "| Hyper Skill |\n" +
             " •••••••••••••")
 }
+*/
