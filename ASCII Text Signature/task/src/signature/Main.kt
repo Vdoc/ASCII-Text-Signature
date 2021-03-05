ffedb70fde0a371a840585de9e9530c4214a281f
NikoBolt <docnikbolt@gmail.com>
2021-03-06 02:24:57 +0300

    Stage 3/4: Make it notable

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,10 +1,230 @@
 package signature
 
 import java.util.*
+/*
+Stage 3/4: Make it notable
+ */
+fun main() {
+    val data = readInpt()
+    printText(data)
+}
+
+fun readInpt(): Array<String> {
+
+    print("Enter name and surname: ")
+    val scanner = Scanner(System.`in`)
+    val name = scanner.nextLine()
+
+    print("Enter person's status: ")
+    val status = scanner.nextLine()
+    return arrayOf(name, status)
+}
+
+fun printText(array: Array<String>) {
+    val output = Out(array)
+    output.addStars()
+    output.addName()
+    output.addStatus()
+    output.addStars()
+    output.print()
+}
+
+class Out {
+    val name: String
+    var lenName: Int = 0
+    var indentName: Int = 0
+    var indentNameLeft: Int = 0
+    var indentNameRight: Int = 0
+
+    val status: String
+    val lenStatus: Int
+    var indentStatus: Int = 0
+    var indentStatusLeft: Int = 0
+    var indentStatusRight: Int = 0
+
+    var lenMax: Int
+    val border: Int = 2
+    val nameFont: Array<Simbol>
+    var put: String = ""
+
+    constructor(array: Array<String>) {
+        name = array[0]
+        nameFont = initStringNameByFontAndCountLengthName()
+
+        status = array[1]
+        lenStatus = status.length
+
+        lenMax = if (lenName > lenStatus) {
+            indentStatus = lenName - lenStatus
+            indentStatusLeft = indentStatus / 2
+            indentStatusRight = indentStatus - indentStatusLeft
+            (lenName + 6)
+        } else {
+            indentName = lenStatus  - lenName
+            indentNameLeft = indentName / 2
+            indentNameRight = indentName - indentNameLeft
+            (lenStatus + 6)
+        }
+    }
+
+    private fun initStringNameByFontAndCountLengthName(): Array<Simbol> {
+        var arr: Array<Simbol> = emptyArray<Simbol>()
+        for (i in 0 until name.length) {
+            arr += Simbol(name[i])
+            lenName += arr[i].lengthOfChar
+        }
+        lenName += name.length - 1    // + spaces between simbols
+        return arr
+    }
+
+    fun addName() {
+        for (i in 0..2) {
+            addLeftBorder(border + indentNameLeft)
+            addStringName(i)
+            addRightBorder(indentNameRight + border)
+        }
+    }
+
+    private fun addStringName(nomberOfString: Int) {
+        put += nameFont[0].chr[nomberOfString]
+        for (i in 1 until nameFont.size) {
+            put += " " + nameFont[i].chr[nomberOfString]
+        }
+    }
+
+    fun addStatus() {
+        addLeftBorder(border + indentStatusLeft)
+        addStringStatus()
+        addRightBorder(indentStatusRight + border)
+    }
+
+    private fun addStringStatus() {
+        put += status
+    }
+
+    private fun addLeftBorder(indent: Int = 2) {
+        put += "*"
+        repeat(indent) {
+            put += " "
+        }
+    }
+
+    private fun addRightBorder(indent: Int = 2) {
+        repeat(indent) {
+            put += " "
+        }
+        put += "*\n"
+    }
+
+    fun addStars() {
+        repeat(lenMax) {
+            put += '*'
+        }
+        put += "\n"
+    }
+
+    fun print() = print(put)
+}
+
+class Simbol {
+    val ch: Char
+    val chr: Array<String>
+    val lengthOfChar: Int
+
+    constructor(ch: Char) {
+        this.ch = ch
+        chr = getFont()
+        lengthOfChar = chr[0].length
+    }
+
+    fun getFont() = when (ch.toUpperCase()) {
+        'A' -> arrayOf( "____",
+                        "|__|",
+                        "|  |")
+        'B' -> arrayOf( "___ ",
+                        "|__]",
+                        "|__]")
+        'C' -> arrayOf( "____",
+                        "|   ",
+                        "|___")
+        'D' -> arrayOf( "___ ",
+                        "|  \\",
+                        "|__/")
+        'E' -> arrayOf( "____",
+                        "|___",
+                        "|___")
+        'F' -> arrayOf( "____",
+                        "|___",
+                        "|   ")
+        'G' -> arrayOf( "____",
+                        "| __",
+                        "|__]")
+        'H' -> arrayOf( "_  _",
+                        "|__|",
+                        "|  |")
+        'I' -> arrayOf( "_",
+                        "|",
+                        "|")
+        'J' -> arrayOf( " _",
+                        " |",
+                        "_|")
+        'K' -> arrayOf( "_  _",
+                        "|_/ ",
+                        "| \\_")
+        'L' -> arrayOf( "_   ",
+                        "|   ",
+                        "|___")
+        'M' -> arrayOf( "_  _",
+                        "|\\/|",
+                        "|  |")
+        'N' -> arrayOf( "_  _",
+                        "|\\ |",
+                        "| \\|")
+        'O' -> arrayOf( "____",
+                        "|  |",
+                        "|__|")
+        'P' -> arrayOf( "___ ",
+                        "|__]",
+                        "|   ")
+        'Q' -> arrayOf( "____",
+                        "|  |",
+                        "|_\\|")
+        'R' -> arrayOf( "____",
+                        "|__/",
+                        "|  \\")
+        'S' -> arrayOf( "____",
+                        "[__ ",
+                        "___]")
+        'T' -> arrayOf( "___",
+                        " | ",
+                        " | ")
+        'U' -> arrayOf( "_  _",
+                        "|  |",
+                        "|__|")
+        'V' -> arrayOf( "_  _",
+                        "|  |",
+                        " \\/ ")
+        'W' -> arrayOf( "_ _ _",
+                        "| | |",
+                        "|_|_|")
+        'X' -> arrayOf( "_  _",
+                        " \\/ ",
+                        "_/\\_")
+        'Y' -> arrayOf( "_   _",
+                        " \\_/ ",
+                        "  |  ")
+        'Z' -> arrayOf( "___ ",
+                        "  / ",
+                        " /__")
+        else -> arrayOf("    ",
+                        "    ",
+                        "    ")
+    }
+}
 
 /*
 Stage 2/4: Star quality
- */
+
 fun main() {
     val scanner = Scanner(System.`in`)
     val str = scanner.nextLine()
@@ -27,12 +247,11 @@ fun printStr1(str: String) {
     }
     print(output)
 }
-
+*/
 
 
 /*
 Stage 1/4: Primitive Art
-
 fun main() {
     println(" _____________\n" +
             "| Hyper Skill |\n" +
